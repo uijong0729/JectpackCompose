dfe80693cb5c8a6e06b2eadfe2301df69ce66bd1
uijong0729 <krocha0729@gmail.com>
2022-11-19 12:42:49 +0900

    floating버튼

--- 3_Todo/app/src/main/java/com/example/todo/MainActivity.kt
+++ 3_Todo/app/src/main/java/com/example/todo/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.todo
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.todo.ui.theme.TodoTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,9 +28,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    mainContent(this@MainActivity)
                 }
             }
         }
+    }
+}
+
+@Composable
+fun mainContent(context: Context) {
+    // 플로팅 액션버튼을 쓰기위한
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = {
+            Toast.makeText(context, "onClick", Toast.LENGTH_LONG).show()
+        }) {
+            // UI
+            Icon(imageVector = Icons.Default.Add, contentDescription = "신규작성")
+        }
+    }) {
+
     }
 }
